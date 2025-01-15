b001b9042748ffc86a0ffbe0921d718f92d7b4e5
Mateusz Cieplak <m.cieplak97@gmail.com>
2025-01-15 21:12:46 +0100

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const largeImage = document.querySelector('.gallery__large-img');
+
+const imageList = document.querySelector('.gallery__list');
+
+const links = document.querySelectorAll('.list-item__link');
+
+links.forEach((link) => {
+  link.addEventListener('click', linkClick);
+});
+
+imageList.addEventListener('click', function (element) {
+  if (element.target.tagName === 'IMG') {
+    largeImage.setAttribute('src', element.target.src);
+  }
+
+  if (element.target.tagName === 'A') {
+    largeImage.setAttribute('href', element.target.href);
+  }
+});
+
+function linkClick(element) {
+  element.preventDefault();
+}
