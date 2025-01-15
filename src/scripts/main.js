f6559ee55702421565e4728bb5b3d7f1f4f347c3
Mateusz Cieplak <m.cieplak97@gmail.com>
2025-01-15 21:26:37 +0100

    Correcting the code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@
 
 const largeImage = document.querySelector('.gallery__large-img');
 
-const imageList = document.querySelector('.gallery__list');
+const smallImages= document.querySelectorAll('.gallery__thumb');
 
 const links = document.querySelectorAll('.list-item__link');
 
@@ -10,14 +10,10 @@ links.forEach((link) => {
   link.addEventListener('click', linkClick);
 });
 
-imageList.addEventListener('click', function (element) {
-  if (element.target.tagName === 'IMG') {
+smallImages.forEach((image) => {
+  image.addEventListener('click', function (element) {
     largeImage.setAttribute('src', element.target.src);
-  }
-
-  if (element.target.tagName === 'A') {
-    largeImage.setAttribute('href', element.target.href);
-  }
+  });
 });
 
 function linkClick(element) {
